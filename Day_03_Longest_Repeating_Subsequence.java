5398f753225b240ae3a2c94afb5fec00235eeb29
himanshups <74396707+himanshups@users.noreply.github.com>
2023-07-18 08:20:26 +0530

    Add files via upload

---
+++ Day_03_Longest_Repeating_Subsequence.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String str = br.readLine().trim();
+            Solution ob = new Solution();
+            int ans = ob.LongestRepeatingSubsequence(str);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+public int LongestRepeatingSubsequence(String str){
+         
+        int n=str.length();
+        int[][] dp =new int[n+1][n+1];
+        
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(str.charAt(i-1)==str.charAt(j-1) && (i-1)!=(j-1)){
+                    dp[i][j]= 1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]= Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][n];
+     }
+}
